f88d781bc92c6382a9e341eacedc36612d88b553
userman123 <dominic.freeman123@gmail.com>
2019-07-22 15:53:02 +0100

    Install plugins and import data from contentful into basic app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import * as contentful from 'contentful';
+require('dotenv').config()
+
+var client = contentful.createClient({
+  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
+})
+
+var moment = require('moment');
 
 function App() {
+  const [academies, setAcademies] = useState([])
+
+  useEffect(() => {
+    client.getEntries({
+      content_type: 'filmAcademy',
+      order: 'fields.location'
+    }).then(entries => {
+      setAcademies(entries.items)
+    })
+
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tbody>
+          <tr>
+            <th>Location</th>
+            <th>Organisation</th>
+            <th>Enquiries</th>
+            <th>Application closing date</th>
+            <th>Course start date</th>
+            <th>Course end date</th>
+          </tr>
+          {academies.map(academy =>
+            <tr>
+              <td>{academy.fields.location}</td>
+              <td>{academy.fields.organisation}</td>
+              <td>{academy.fields.contactEmail}</td>
+              <td>{moment(academy.fields.deadline).format("Do MMM YYYY")}</td>
+              <td>{moment(academy.fields.startDate).format("Do MMM YYYY")}</td>
+              <td>{moment(academy.fields.endDate).format("Do MMM YYYY")}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
